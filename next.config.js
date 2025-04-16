6191e7083780787ae8747ebad36f7ff20cfd224a
Tim <tyoung560@gmail.com>
2025-04-16 16:42:30 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Don't resolve Node.js built-in modules on the client side
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        crypto: false,
+        stream: false,
+        os: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
